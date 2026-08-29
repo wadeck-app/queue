0cb772e1a4087141646071935889e5660c63626a
Wadeck <wadeck.follonier@gmail.com>
2026-08-29 09:36:17 +0200

    chore(vitest): enable global test variables and set environment to node

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
   test: { globals: true, environment: 'node', restoreMocks: true, include: ['src/**/*.test.ts'] }
 });
